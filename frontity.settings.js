88ebd2d8c8c202478c27bb0dd11150bf6429a7e1
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-02-05 14:58:22 +0000

    First commit - set up with Frontity

---
+++ frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "gfw-blog-redesign",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.io",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://dev-global-forest-watch-blog.pantheonsite.io/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
