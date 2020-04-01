4bba81bb14c1a0ef15df36b642db36ca3441333b
Ed Brett <e.j.a.brett@gmail.com>
2020-04-01 21:53:14 +0200

    fix issues with yarn and gfw-components

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { Global, css, connect, styled, Head } from "frontity";
-import Switch from "@frontity/components/switch";
-// import Header from "./header";
+import React from 'react';
+import { Global, css, connect, styled, Head } from 'frontity';
+import PropTypes from 'prop-types';
+
+import Switch from '@frontity/components/switch';
+
 import gfwUIStyles from 'gfw-components/dist/main.css';
 import { Header } from 'gfw-components';
-import List from "./list";
-import Post from "./post";
-import Loading from "./loading";
-import Title from "./title";
-import PageError from "./page-error";
 
-const GFWComponenentsStyles = () => (
-  <Global styles={css(gfwUIStyles)} />
-);
+import List from './list';
+import Post from './post';
+import Loading from './loading';
+import Title from './title';
+import PageError from './page-error';
+
+const GFWComponenentsStyles = () => <Global styles={css(gfwUIStyles)} />;
 
 /**
  * Theme is the root React component of our theme. The one we will export
@@ -33,13 +34,10 @@ const Theme = ({ state }) => {
 
       {/* Add some global styles for the whole site, like body or a's.
       Not classes here because we use CSS-in-JS. Only global HTML tags. */}
-      <Global styles={globalStyles} />
       <GFWComponenentsStyles />
 
       {/* Add the header of the site. */}
-      <HeadContainer>
-        <Header />
-      </HeadContainer>
+      <Header />
 
       {/* Add the main section. It renders a different component depending
       on the type of URL we are in. */}
@@ -55,27 +53,11 @@ const Theme = ({ state }) => {
   );
 };
 
+Theme.propTypes = {
+  state: PropTypes.object,
+};
+
 export default connect(Theme);
-
-const globalStyles = css`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-      "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  }
-  a,
-  a:visited {
-    color: inherit;
-    text-decoration: none;
-  }
-`;
-
-const HeadContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: #1f38c5;
-`;
 
 const Main = styled.div`
   display: flex;
