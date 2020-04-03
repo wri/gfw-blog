1d86711b66053f0987d549cfe1386427f7819a30
Ed Brett <e.j.a.brett@gmail.com>
2020-04-03 16:26:32 +0200

    add favicon and rename site

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,59 +1,38 @@
 import { WORDPRESS_GFW_API } from './constants';
 
 const settings = {
-  "name": "gfw-blog-redesign",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.io",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: 'gfw-blog',
+  state: {
+    frontity: {
+      url: 'https://blog.globalforestwatch.org',
+      title: 'Global Forest Watch Blog',
+      description:
+        'The GFW blog catalyzes conversations around improved forest management by providing timely, credible analysis on threats to global forests.',
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
-          ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+      name: '@frontity/mars-theme',
+      state: {
+        theme: {
+          featured: {
+            showOnList: false,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": WORDPRESS_GFW_API
-        }
-      }
+      name: '@frontity/wp-source',
+      state: {
+        source: {
+          api: WORDPRESS_GFW_API,
+        },
+      },
     },
-    "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    '@frontity/tiny-router',
+    '@frontity/html2react',
+  ],
 };
 
 export default settings;
