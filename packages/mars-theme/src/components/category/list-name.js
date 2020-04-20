6137d42084e3a279ca629a3b6f5d2a3facc59cbf
Pavlo Mohyla <pavlo.mohyla@p2h.com>
2020-04-20 22:38:46 +0300

    Updated categoty components

--- packages/mars-theme/src/components/category/list-name.js
+++ packages/mars-theme/src/components/category/list-name.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { styled } from 'frontity';
+import { styled, css } from 'frontity';
 import CategoryName from './name';
+import Link from '../link';
+import { SMALL_ENDPOINT, MEDIUM_ENDPOINT } from '../heplers/css-endpoints';
 
-const CategoryNameList = ({ categories, styles = '' }) => {
+const CategoryNameList = ({ categories, styles = '', title }) => {
   const Wrapper = styled.p`
-      padding: 1rem 0;
-      ${styles}
-    `;
+    @media screen and (max-width: ${MEDIUM_ENDPOINT}) {
+      padding: 0 1rem;
+    }
+    ${styles}
+  `;
+  const linkCss = `
+    color: #fff;
+  `;
   return (
-    <Wrapper>
-      {categories.map(name => {
-        return <CategoryName key={name}>{name}</CategoryName>
+    <Wrapper className="categories-list-wrapper">
+      {title && <TitleWrapper>{title}</TitleWrapper>}
+      {categories.map(({ name, link }) => {
+        return (
+          <CategoryName key={name}>
+            <Link
+              css={css`
+                ${linkCss}
+              `}
+              link={link}
+            >
+              {name}
+            </Link>
+          </CategoryName>
+        );
       })}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CategoryNameList
+export default CategoryNameList;
 
 CategoryNameList.propTypes = {
   categories: PropTypes.array,
   styles: PropTypes.string,
-}
+  title: PropTypes.string,
+};
+
+const TitleWrapper = styled.span`
+  margin-right: 1rem;
+  @media screen and (max-width: ${SMALL_ENDPOINT}) {
+    display: block;
+  }
+`;
--- packages/mars-theme/src/components/category/name.js
+++ packages/mars-theme/src/components/category/name.js
@@ -1,27 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { styled, decode } from "frontity";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { styled, decode } from 'frontity';
 
-const CategoryName = ({ children, styles='' }) => {
+const CategoryName = ({ children, styles = '' }) => {
   const Wrapper = styled.span`
     background-color: rgba(51, 51, 51, 1);
     color: #fff;
     padding: 0.5rem 1rem;
     font-size: 0.9em;
     font-weight: 200;
-    margin-right: 0.5em;
     text-transform: uppercase;
+    display: inline-flex;
+    margin-right: 1em;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
     ${styles}
   `;
   return (
     <Wrapper>
       <b>{decode(children)}</b>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CategoryName
+export default CategoryName;
 
 CategoryName.propTypes = {
   styles: PropTypes.string,
-}
+  children: PropTypes.node,
+};
