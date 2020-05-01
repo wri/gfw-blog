76ec48c9989814c0b92d76ff1327b0ffcff17dfc
Pavlo Mohyla <pavlo.mohyla@p2h.com>
2020-05-01 17:34:06 +0300

    Setup components for different entities info section

---
+++ packages/mars-theme/src/components/entity-info/category-list.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'frontity';
+import { Item } from './components';
+import Link from '../link';
+
+const CATEGORIES_PER_PAGE = 10;
+
+const CategoryList = ({ state, libraries, handler }) => {
+  const data = state.source.get(state.router.link);
+  const { api } = libraries.source;
+  const totalNumber = Object.values(state.source.category).length;
+
+  const [page, setPage] = useState(0);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const fetchCategories = useCallback(() => {
+    setIsFetching(true);
+    if (page) {
+      const result = api.get({
+        endpoint: 'categories',
+        params: { _embed: true, page, orderby: 'count', order: 'desc' },
+      });
+      result.then((response) => {
+        libraries.source.populate({ response, state, force: true });
+      });
+    }
+  }, [page, setIsFetching]);
+
+  useEffect(() => {
+    if (page && !isFetching) {
+      fetchCategories(page);
+    }
+  }, [page, isFetching]);
+
+  useEffect(() => {
+    if (totalNumber < CATEGORIES_PER_PAGE) {
+      setPage(page + 1);
+    }
+  }, []);
+
+  const [categories, setCategories] = useState(
+    Object.values(state.source.category).sort((prev, next) => {
+      return next.count - prev.count;
+    })
+  );
+
+  useEffect(() => {
+    const allCategories = Object.values(state.source.category)
+      .filter((cat) => cat.count)
+      .sort((prev, next) => {
+        return next.count - prev.count;
+      });
+    setCategories(allCategories);
+  }, [totalNumber, setCategories, setPage]);
+
+  return categories.map((cat) => {
+    if (!cat.count) {
+      return null;
+    }
+    return (
+      <Item
+        key={cat.name + cat.id}
+        className={data.id === cat.id ? `current` : ``}
+        onClick={handler}
+      >
+        <Link link={cat.link}>
+          {cat.name}
+          &nbsp; (
+          {cat.count}
+          )
+        </Link>
+      </Item>
+    );
+  });
+};
+
+export default connect(CategoryList);
+
+CategoryList.propTypes = {
+  state: PropTypes.object,
+  libraries: PropTypes.object,
+  handler: PropTypes.func,
+};
---
+++ packages/mars-theme/src/components/entity-info/description.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect, styled, decode } from 'frontity';
+
+const EntityDescription = ({ state }) => {
+  const data = state.source.get(state.router.link);
+  const description = decode(state.source[data.taxonomy][data.id].description);
+  return (
+    <>
+      {data.isCategory && (
+        <>
+          {description && <Title>{description}</Title>}
+          <NumberInfo>
+            {`${state.source[data.taxonomy][data.id].count}
+            articles under the
+            ${decode(state.source[data.taxonomy][data.id].name)}
+            category`}
+          </NumberInfo>
+        </>
+      )}
+    </>
+  );
+};
+
+export default connect(EntityDescription);
+
+EntityDescription.propTypes = {
+  state: PropTypes.object,
+};
+
+const Title = styled.div`
+  font-size: 1.125rem;
+  line-height: 1.875rem;
+  color: var(--color-dark-grey);
+  padding-top: 1.625rem;
+`;
+
+const NumberInfo = styled.div`
+  font-size: 0.875rem;
+  line-height: 1.3125rem;
+  margin-top: 4.625rem;
+  color: var(--color-medium-grey);
+`;
