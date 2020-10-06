09833d11899f9709a64c924c60d29e07589efc99
Ed Brett <e.j.a.brett@gmail.com>
2020-10-06 09:03:58 +0100

    remove unwanted components

--- components/related-content/articles/styles.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import styled from '@emotion/styled';
-
-export const LinkWrapper = styled.a`
-  display: block;
-  margin-bottom: 30px;
-  cursor: pointer;
-`;
--- components/related-content/organizations/styles.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import styled from '@emotion/styled';
-
-export const CardWrapper = styled.div`
-  display: block;
-  margin-bottom: 30px;
-`;
