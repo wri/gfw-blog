422085d503ef9029faaa53d6e7e12946cf892399
Ed Brett <e.j.a.brett@gmail.com>
2020-10-08 18:59:10 +0100

    add try catch for tags fallback

--- pages/tag/[tag].js
+++ pages/tag/[tag].js
@@ -14,32 +14,40 @@ export default function Tag(props) {
 }
 
 export async function getStaticProps({ params }) {
-  const tags = await getTags({
-    params: { per_page: 50, orderby: 'count', order: 'desc' },
-  });
-  const tag = await getTagBySlug({ slug: params.tag });
+  try {
+    const tags = await getTags({
+      params: { per_page: 50, orderby: 'count', order: 'desc' },
+    });
+    const tag = await getTagBySlug({ slug: params.tag });
 
-  const postsResponse = await getPostsByType({
-    type: 'posts',
-    params: {
-      tags: tag?.id,
-      per_page: 12,
-    },
-  });
+    const postsResponse = await getPostsByType({
+      type: 'posts',
+      params: {
+        tags: tag?.id,
+        per_page: 12,
+      },
+    });
 
-  return {
-    props: {
-      taxType: 'tags',
-      tax: tag || null,
-      allTax: tags || [],
-      posts: postsResponse?.posts || [],
-      totalPages: postsResponse?.totalPages || 1,
-      totalPosts: postsResponse?.total || 0,
-      metaTags: tag?.yoast_head || '',
-      isError: !tag,
-    },
-    revalidate: 10,
-  };
+    return {
+      props: {
+        taxType: 'tags',
+        tax: tag || null,
+        allTax: tags || [],
+        posts: postsResponse?.posts || [],
+        totalPages: postsResponse?.totalPages || 1,
+        totalPosts: postsResponse?.total || 0,
+        metaTags: tag?.yoast_head || '',
+        isError: !tag,
+      },
+      revalidate: 10,
+    };
+  } catch (err) {
+    return {
+      props: {
+        isError: true,
+      },
+    };
+  }
 }
 
 export async function getStaticPaths() {
