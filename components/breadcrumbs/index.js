7e84defd8b455b57b50e6de5819bf0a3897b6cbe
Ed Brett <e.j.a.brett@gmail.com>
2020-10-12 21:39:29 +0100

    no translate for breadcrumbs

--- components/breadcrumbs/index.js
+++ components/breadcrumbs/index.js
@@ -7,7 +7,7 @@ import Wrapper from './styles';
 
 const Breadcrumbs = ({ links }) => {
   return (
-    <Wrapper>
+    <Wrapper className="notranslate">
       <Link href="/">Blog home</Link>
       {links?.map(({ label, ...link }) => (
         <Fragment key={label}>
