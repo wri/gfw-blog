3e2a1dfc101c976c7f2863e9d13d866664925cb5
Tomas Eriksson <tomasmoosee@gmail.com>
2021-05-18 17:23:08 +0200

    make sure og:url is correct

--- utils/content.js
+++ utils/content.js
@@ -9,6 +9,10 @@ export const getLessContent = (str) => {
 export const serializeYoast = (yoast) => {
   return yoast
     .replace(
+      /https:\/\/www.globalforestwatch.org\/(blog\/?)?/g, // /blog/ might not be present, and has to be added if thats the case.
+      'https://www.globalforestwatch.org/blog/'
+    )
+    .replace(
       /https:\/\/content.globalforestwatch.org\/global-forest-watch-blog/g,
       'https://www.globalforestwatch.org/blog'
     )
