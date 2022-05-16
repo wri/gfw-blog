a9820aaf6df7d3f1d9d51a5d1373c5308dae1077
Pedro Pimenta <pedro@pimenta.co>
2022-05-16 13:24:26 +0200

    Fix slug check

--- pages/[...slugs].js
+++ pages/[...slugs].js
@@ -88,7 +88,7 @@ export async function getStaticProps({ params }) {
       slug,
     });
 
-    if (!post || post?.link !== `/${slugs?.join('/')}`) {
+    if (!post || post?.link !== `/${slugs?.join('/')}/`) {
       return {
         props: {
           isError: true,
