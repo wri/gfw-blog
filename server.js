01801fd56ca4992e769959a0d752fe3afd2cf843
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-12-07 14:29:51 +0000

    Revert "test domain redirect"
    This reverts commit 98d7069bb74a5c860f3ebc1f403680c5b778ef7d.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const next = require('next');
 const express = require('express');
 const sslRedirect = require('heroku-ssl-redirect').default;
 
-const port = parseInt(process.env.PORT, 10) || 9090;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
