548a17d1ca6340be0bc0b100b181cb7e1f677563
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-11-22 14:45:30 -0600

    feat(Slider): Add slider component

---
+++ components/slider/index.js
@@ -0,0 +1,90 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import Card from 'components/card';
+import LeftArrowIcon from 'assets/icons/left-arrow.svg';
+import RightArrowIcon from 'assets/icons/right-arrow.svg';
+import SliderWrapper from './styles';
+
+/**
+ * see: https://stackoverflow.com/a/52747760
+ * @param {*} t current time
+ * @param {*} b start value
+ * @param {*} c change in value
+ * @param {*} d duration
+ * @returns
+ */
+Math.easeInOutQuad = (t, b, c, d) => {
+  t /= d / 2;
+
+  if (t < 1) {
+    return (c / 2) * t * t + b;
+  }
+
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
+
+const scroll = (element, change, duration) => {
+  const start = element.scrollLeft;
+  const increment = 20;
+  let currentTime = 0;
+
+  const animateScroll = () => {
+    currentTime += increment;
+    const value = Math.easeInOutQuad(currentTime, start, change, duration);
+
+    element.scrollLeft = value;
+
+    if (currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+
+  animateScroll();
+};
+
+const Slider = ({ cards, withBackgroundImage = false, title }) => {
+  const sliderRef = useRef();
+  const cardTextColor = withBackgroundImage ? 'white' : undefined;
+
+  const handleScrollRight = () => {
+    scroll(sliderRef.current, 300, 100);
+  };
+
+  const handleScrollLeft = () => {
+    scroll(sliderRef.current, -300, 100);
+  };
+
+  return (
+    <SliderWrapper withBackgroundImage={withBackgroundImage}>
+      <div className="title">{title}</div>
+      <div className="slider">
+        <div ref={sliderRef} className="slides">
+          {cards &&
+            cards.map((card) => (
+              <div key={card.id} className="slide">
+                <Card textColor={cardTextColor} {...card} excerpt="" large />
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="button" onClick={handleScrollLeft}>
+          <LeftArrowIcon />
+        </button>
+        <button className="button" onClick={handleScrollRight}>
+          <RightArrowIcon />
+        </button>
+      </div>
+    </SliderWrapper>
+  );
+};
+
+Slider.propTypes = {
+  cards: PropTypes.array,
+  withBackgroundImage: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+export default Slider;
