7c99f72d7b44cf385b157a1707afe31777be93a5
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-11-23 15:41:31 -0600

    feat(Card): Display golden icon when article is external link

--- components/media/styles.js
+++ components/media/styles.js
@@ -3,6 +3,14 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   height: 100%;
   width: 100%;
+
+  .icon {
+    position: absolute;
+    z-index: 1;
+    bottom: -4px;
+    left: -1px;
+    cursor: pointer;
+  }
 `;
 
 export const StyledImage = styled.img`
