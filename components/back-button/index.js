d7d44733baa7190098c8182fc138a1a1ec2979ce
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-12-07 13:00:30 -0600

    feat(back-button): Add BackButton reusable component

---
+++ components/back-button/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BackButtonWrapper, GreenLeftArrowIcon } from './styles';
+
+const BackButton = ({ handleClick, title }) => {
+  return (
+    <BackButtonWrapper>
+      <button onClick={handleClick}>
+        <GreenLeftArrowIcon />
+        <span className="title">{title}</span>
+      </button>
+    </BackButtonWrapper>
+  );
+};
+
+BackButton.propTypes = {
+  handleClick: PropTypes.func,
+  title: PropTypes.string,
+};
+
+export default BackButton;
