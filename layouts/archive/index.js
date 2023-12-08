23133dcf1bef9241d1285d24d837e7d4d0b235ec
Willian Viana <willian.batista.viana@gmail.com>
2023-12-08 16:11:55 -0300

    feat(no-results-page): add contact us block with inline css for now

--- layouts/archive/index.js
+++ layouts/archive/index.js
@@ -7,6 +7,7 @@ import {
   theme,
   Loader,
   Button,
+  ContactUsModal,
 } from '@worldresources/gfw-components';
 import compact from 'lodash/compact';
 
@@ -17,6 +18,7 @@ import { translateText } from 'utils/lang';
 import Card from 'components/card';
 import Breadcrumbs from 'components/breadcrumbs';
 import Dropdown from 'components/dropdown';
+// import Slider from 'components/slider'
 
 import {
   Wrapper,
@@ -77,6 +79,7 @@ const ArchivePage = ({
   const [posts, setPosts] = useState(firstPagePosts || []);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     setPosts(firstPagePosts);
@@ -110,119 +113,197 @@ const ArchivePage = ({
   }, [page]);
 
   return (
-    <Wrapper>
-      <Row
-        css={css`
-          position: relative;
-          min-height: 40px;
-        `}
-      >
-        <Column width={[3 / 4]}>
-          <Breadcrumbs
-            css={css`
-              margin-bottom: 25px;
-              ${theme.mediaQueries.small} {
-                margin-bottom: 40px;
-              }
-            `}
-            links={breadCrumbs}
-          />
-        </Column>
-        {!isSearch && (
-          <Column width={[1 / 4]}>
-            <SearchMobile expandable />
-          </Column>
-        )}
-        {isSearch && (
-          <>
-            <Column>
-              <SearchDesktop expanded isSearch />
-            </Column>
-          </>
-        )}
-      </Row>
-      {!isSearch && (
+    <>
+      <Wrapper>
         <Row
           css={css`
             position: relative;
+            min-height: 40px;
           `}
         >
-          <Column width={[1, 2 / 3]}>
-            <Dropdown items={allTaxOptions} selected={tax?.id} />
-          </Column>
-          <Column width={[1, 1 / 3]}>
-            <SearchDesktop showTitle expandable />
-          </Column>
-          {tax?.description && (
-            <Column
-              width={[1, 3 / 4]}
+          <Column width={[3 / 4]}>
+            <Breadcrumbs
               css={css`
-                margin-bottom: 20px !important;
+                margin-bottom: 25px;
+                ${theme.mediaQueries.small} {
+                  margin-bottom: 40px;
+                }
               `}
-            >
-              <CategoryDescription>{tax.description}</CategoryDescription>
+              links={breadCrumbs}
+            />
+          </Column>
+          {!isSearch && (
+            <Column width={[1 / 4]}>
+              <SearchMobile expandable />
             </Column>
           )}
-        </Row>
-      )}
-      <Row>
-        <Column
-          css={css`
-            margin-bottom: 20px !important;
-          `}
-        >
-          <ResultsStatement>
-            {translateText(resultsStatement, { totalPosts })}
-          </ResultsStatement>
-        </Column>
-        {posts?.map(({ id, ...rest }) => (
-          <Column
-            width={[1, 1 / 2, 1 / 3]}
-            css={css`
-              margin-bottom: 40px !important;
-            `}
-            key={id}
-          >
-            <Card {...rest} />
-          </Column>
-        ))}
-        <Column>
-          <Row nested>
-            <Column width={[1 / 12, 1 / 3]} />
-            <LoadMoreWrapper width={[5 / 6, 1 / 3]}>
-              {loading && (
-                <div
-                  style={{
-                    position: 'relative',
-                    width: '50px',
-                    height: '50px',
-                  }}
-                >
-                  <Loader />
-                </div>
-              )}
-              {!loading && page < totalPages && (
-                <Button
-                  onClick={() => {
-                    setPage(page + 1);
-                    trackEvent({
-                      category: 'GFW Blog',
-                      label: 'User clicks on more articles button',
-                      action: 'Load more articles',
-                    });
-                  }}
+          {isSearch && (
+            <>
+              <Column>
+                <h1
                   css={css`
-                    width: 100%;
+                    font-size: 3.75rem;
                   `}
                 >
-                  Load more articles
-                </Button>
-              )}
-            </LoadMoreWrapper>
+                  No Results for Lorem Ipsum
+                </h1>
+                {/* <SearchDesktop expanded isSearch /> */}
+              </Column>
+            </>
+          )}
+        </Row>
+        {!isSearch && (
+          <Row
+            css={css`
+              position: relative;
+            `}
+          >
+            <Column width={[1, 2 / 3]}>
+              <Dropdown items={allTaxOptions} selected={tax?.id} />
+            </Column>
+            <Column width={[1, 1 / 3]}>
+              <SearchDesktop showTitle expandable />
+            </Column>
+            {tax?.description && (
+              <Column
+                width={[1, 3 / 4]}
+                css={css`
+                  margin-bottom: 20px !important;
+                `}
+              >
+                <CategoryDescription>{tax.description}</CategoryDescription>
+              </Column>
+            )}
           </Row>
+        )}
+        <Row>
+          <Column
+            css={css`
+              margin-bottom: 20px !important;
+            `}
+          >
+            <ResultsStatement>
+              {translateText(resultsStatement, { totalPosts })}
+            </ResultsStatement>
+          </Column>
+          {posts?.map(({ id, ...rest }) => (
+            <Column
+              width={[1, 1 / 2, 1 / 3]}
+              css={css`
+                margin-bottom: 40px !important;
+              `}
+              key={id}
+            >
+              <Card {...rest} />
+            </Column>
+          ))}
+          <Column>
+            <Row nested>
+              <Column width={[1 / 12, 1 / 3]} />
+              <LoadMoreWrapper width={[5 / 6, 1 / 3]}>
+                {loading && (
+                  <div
+                    style={{
+                      position: 'relative',
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  >
+                    <Loader />
+                  </div>
+                )}
+                {!loading && page < totalPages && (
+                  <Button
+                    onClick={() => {
+                      setPage(page + 1);
+                      trackEvent({
+                        category: 'GFW Blog',
+                        label: 'User clicks on more articles button',
+                        action: 'Load more articles',
+                      });
+                    }}
+                    css={css`
+                      width: 100%;
+                    `}
+                  >
+                    Load more articles
+                  </Button>
+                )}
+              </LoadMoreWrapper>
+            </Row>
+          </Column>
+        </Row>
+
+        <Row>
+          {/* <Slider cards={[mainPost, ...subPosts]} title="Featured Articles" /> */}
+        </Row>
+        {/* <Row
+          css={css`
+            ${theme.mediaQueries.small} {
+              display: none;
+            }
+          `}
+        >
+          <Slider
+            cards={relatedPosts}
+            title='Explore More Articles'
+            backgroundImageUrl='../../images/prefooter-mobile.png'
+          />
+        </Row> */}
+      </Wrapper>
+      <Row
+        css={css`
+          background-size: cover;
+          background-image: url('images/hero-bg-mobile.png');
+          max-width: 100%;
+          width: 100%;
+          height: 20.5625rem;
+          ${theme.mediaQueries.small} {
+            background-image: url('images/hero-bg-desktop.png');
+          }
+        `}
+      >
+        <Column
+          css={css`
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+            align-items: center;
+            background-size: cover;
+            background-position: center;
+            background-image: url('../../images/no-results-bg.png');
+          `}
+        >
+          <h2
+            css={css`
+              font-size: 2.5rem;
+              color: #fff;
+              margin-bottom: 2.25rem;
+              font-family: Fira Sans;
+              font-style: normal;
+              font-weight: 400;
+              line-height: 3rem;
+              letter-spacing: 0.01563rem;
+              text-align: center;
+            `}
+          >
+            Not Finding What You&apos;re Looking For?
+          </h2>
+          <Button
+            css={css`
+              color: rgb(151, 190, 50);
+              font-size: 1rem;
+            `}
+            onClick={() => setOpen(!open)}
+            light
+            size="large"
+          >
+            Contact Us
+          </Button>
         </Column>
       </Row>
-    </Wrapper>
+      <ContactUsModal open={open} onRequestClose={() => setOpen(false)} />
+    </>
   );
 };
 
