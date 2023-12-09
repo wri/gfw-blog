2b39315b4d894251e34690d519041474a07a57de
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2023-12-09 12:23:52 -0600

    feat(post): Update layout

--- components/caption/styles.js
+++ components/caption/styles.js
@@ -2,9 +2,10 @@ import styled from '@emotion/styled';
 
 export default styled.span`
   p {
-    color: #555;
+    color: #aaaaaa;
     font-size: 12px;
     line-height: 21px;
     display: inline-block;
+    text-transform: uppercase;
   }
 `;
