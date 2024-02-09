fcb5fa7c71f8f838fe86331c3ceb7e0a3402befe
Willian Viana <willian.batista.viana@gmail.com>
2024-02-09 17:40:08 -0300

    feat(components): add dumb components to avoid large layout files

---
+++ components/explore-more/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+
+import { Column, Row, theme } from '@worldresources/gfw-components';
+import Card, { CARD_MEDIA_SIZE } from 'components/card';
+
+import { MoreArticlesWrapper, LatestTitle } from './styles';
+
+const ExploreMore = ({ moreArticles }) => {
+  return (
+    <MoreArticlesWrapper>
+      <Row
+        css={css`
+          padding: 1.25rem 0;
+        `}
+      >
+        <Column>
+          <LatestTitle>Explore More Articles</LatestTitle>
+        </Column>
+        {moreArticles &&
+          moreArticles.map((p) => (
+            <Column
+              width={[1, 1 / 2, 1 / 3]}
+              css={css`
+                margin-bottom: 2.5rem !important;
+              `}
+              key={p?.id}
+            >
+              <Card
+                {...p}
+                large
+                textColor="black"
+                imageSize={`
+                  height: ${CARD_MEDIA_SIZE.MOBILE.height};
+
+                  ${theme.mediaQueries.small} {
+                    height: ${CARD_MEDIA_SIZE.MEDIUM.height};
+                  }
+                `}
+              />
+            </Column>
+          ))}
+      </Row>
+    </MoreArticlesWrapper>
+  );
+};
+
+ExploreMore.propTypes = {
+  moreArticles: PropTypes.array,
+};
+
+export default ExploreMore;
