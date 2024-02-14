9f1d109035342e4436ec9c1969acd6e44d94cb0b
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2024-02-14 17:13:57 -0600

    feat(filter): filter dropdown and pills

--- components/filter/styles.js
+++ components/filter/styles.js
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled';
 import { theme, Row } from '@worldresources/gfw-components';
 
+export const Wrapper = styled.div`
+  width: 100%;
+  padding-bottom: 3rem;
+`;
+
 export const FilterByWrapper = styled(Row)`
   display: flex;
   flex-flow: column-reverse;
@@ -16,6 +21,10 @@ export const FilterByWrapper = styled(Row)`
     flex-flow: row;
     height: 1.25rem;
     justify-content: space-between;
+
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: 30rem 30rem;
   }
 `;
 
@@ -41,5 +50,46 @@ export const FilterByTopic = styled.div`
 
   ${theme.mediaQueries.small} {
     text-align: right;
+  }
+`;
+
+export const PillContainerWrapper = styled(Row)`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  padding: 1rem 2rem;
+  width: 100%;
+
+  ${theme.mediaQueries.small} {
+    padding-left: 2.4rem;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: 12rem 30rem;
+  }
+
+  .category {
+    padding: 0.25rem;
+    background-color: #95bc3b;
+    color: #ffffff;
+    border-radius: 0.125rem;
+    padding: 0.1rem 0.45rem;
+    text-transform: uppercase;
+    font-size: 0.875rem;
+    line-height: 1.3125rem;
+    font-weight: 500;
+    text-align: left;
+    margin-right: 0.5rem;
+  }
+
+  .topic {
+    padding: 0.25rem;
+    background-color: #333333;
+    color: #ffffff;
+    border-radius: 0.125rem;
+    padding: 0.1rem 0.45rem;
+    text-transform: uppercase;
+    font-size: 0.875rem;
+    line-height: 1.3125rem;
+    font-weight: 500;
+    text-align: left;
+    margin-right: 0.5rem;
   }
 `;
