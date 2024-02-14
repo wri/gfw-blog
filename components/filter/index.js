fc1711d106c3b3f1b7b63f7088d6acaaf1970903
Luis Zenteno <luis.zenteno.5@wriconsultant.org>
2024-02-14 17:13:57 -0600

    feat(filter): filter dropdown and pills

--- components/filter/index.js
+++ components/filter/index.js
@@ -1,36 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
 import FilterArrowIcon from 'assets/icons/filter-arrow.svg';
+import FilterArrowUpIcon from 'assets/icons/filter-arrow-up.svg';
+import CloseWhiteIcon from 'assets/icons/close-white.svg';
 import { Column } from '@worldresources/gfw-components';
 
+import FilterDropDown from 'components/filter-dropdown';
 import {
+  Wrapper,
   FilterByWrapper,
   FilterByColumn,
   FilterByTopic,
   FilterByCategory,
+  PillContainerWrapper,
 } from './styles';
 
-const Filter = ({ children }) => {
+const Filter = ({
+  children,
+  categories,
+  topics,
+  selectedCategories,
+  selectedTopics,
+  handleSelectedCategory,
+  handleSelectedTopic,
+}) => {
+  const [isTopicsDrodownOpen, setIsTopicsDropdownOpen] = useState(false);
+  const [isCategoriesDrodownOpen, setIsCategoriesDropdownOpen] = useState(
+    false
+  );
+
+  const handleTopicsOpen = () => {
+    setIsTopicsDropdownOpen(!isTopicsDrodownOpen);
+    setIsCategoriesDropdownOpen(false);
+  };
+
+  const handleCategoriesOpen = () => {
+    setIsCategoriesDropdownOpen(!isCategoriesDrodownOpen);
+    setIsTopicsDropdownOpen(false);
+  };
+
   return (
-    <FilterByWrapper>
-      <Column
-        css={css`
-          display: flex;
-        `}
-      >
-        <FilterByColumn>
-          <FilterByCategory>FILTER BY CATEGORY</FilterByCategory>
-          <FilterArrowIcon />
-        </FilterByColumn>
-        <FilterByColumn>
-          <FilterByTopic>FILTER BY TOPIC</FilterByTopic>
-          <FilterArrowIcon />
-        </FilterByColumn>
-      </Column>
-      <Column>{children}</Column>
-    </FilterByWrapper>
+    <Wrapper>
+      <FilterByWrapper>
+        <Column
+          css={css`
+            display: flex;
+          `}
+        >
+          <FilterByColumn>
+            <FilterByCategory>FILTER BY CATEGORY</FilterByCategory>
+            <button onClick={handleCategoriesOpen}>
+              {isCategoriesDrodownOpen ? (
+                <FilterArrowUpIcon />
+              ) : (
+                <FilterArrowIcon />
+              )}
+            </button>
+          </FilterByColumn>
+          <FilterByColumn>
+            <FilterByTopic>FILTER BY TOPIC</FilterByTopic>
+            <button onClick={handleTopicsOpen}>
+              {isTopicsDrodownOpen ? (
+                <FilterArrowUpIcon />
+              ) : (
+                <FilterArrowIcon />
+              )}
+            </button>
+          </FilterByColumn>
+        </Column>
+        <Column>{children}</Column>
+      </FilterByWrapper>
+      {isCategoriesDrodownOpen && (
+        <FilterDropDown
+          items={categories}
+          selectedItems={selectedCategories}
+          handleSelectItem={handleSelectedCategory}
+          margin="1rem"
+        />
+      )}
+      {isTopicsDrodownOpen && (
+        <FilterDropDown
+          items={topics}
+          selectedItems={selectedTopics}
+          handleSelectItem={handleSelectedTopic}
+          margin="12rem"
+        />
+      )}
+      <PillContainerWrapper>
+        <div>
+          {selectedCategories.map((item) => (
+            <span key={item} className="category">
+              {categories.find((c) => c.slug === item)?.name}
+              &nbsp;
+              <button onClick={() => handleSelectedCategory(item)}>
+                <CloseWhiteIcon />
+              </button>
+            </span>
+          ))}
+        </div>
+        <div>
+          {selectedTopics.map((item) => (
+            <span key={item} className="topic">
+              {topics.find((t) => t.slug === item)?.name}
+              &nbsp;
+              <button onClick={() => handleSelectedTopic(item)}>
+                <CloseWhiteIcon />
+              </button>
+            </span>
+          ))}
+        </div>
+      </PillContainerWrapper>
+    </Wrapper>
   );
 };
 
@@ -39,6 +121,12 @@ Filter.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
+  topics: PropTypes.array,
+  categories: PropTypes.array,
+  selectedCategories: PropTypes.array,
+  selectedTopics: PropTypes.array,
+  handleSelectedTopic: PropTypes.func,
+  handleSelectedCategory: PropTypes.func,
 };
 
 export default Filter;
